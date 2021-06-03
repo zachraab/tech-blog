ef08f455350f9b5e467338ac1909e0ea3ec56d30
Zach Raab <zpraab@hotmail.com>
2021-06-03 02:37:09 -0600

    required in helper js file to server

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const exphbs = require("express-handlebars");
 const path = require("path");
+const helpers = require("./utils/helper");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: "Super secret secret",
