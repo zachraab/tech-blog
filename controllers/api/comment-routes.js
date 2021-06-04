2ce4a2fffa448aacea98d04064c5845cb2d0adb4
Zach Raab <zpraab@hotmail.com>
2021-06-04 09:08:18 -0600

    comments route handler creation, handlebars bug fix

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+Comment.get("/", (req, res) => {
+  try {
+    // Get all comments and JOIN with post data
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: Post,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      comments,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+Comment.post("/", withAuth, (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const postRoutes = require("./post-routes");
 const userRoutes = require("./user-routes");
+const userRoutes = require("./comment-routes");
 
 router.use("/posts", postRoutes);
 router.use("/users", userRoutes);
+router.use("/comments", userRoutes);
 
 module.exports = router;
