8525fd05cb7ff9b7c97e963c2394c8a475c08180
Zach Raab <zpraab@hotmail.com>
2021-06-03 23:44:06 -0600

    comment model creation, post handlebars bug fix

---
+++ models/comment.js
@@ -0,0 +1,39 @@
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,12 @@
 const sequelize = require("../config/connection");
-const { User, Post } = require("../models");
+const { User, Post, Comment } = require("../models");
 
 const userData = require("./userData.json");
 const postData = require("./postData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+  await Comment.sync({ force: true });
 
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
